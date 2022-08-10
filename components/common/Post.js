535315f6d8a616a01d8cbe36971ed529c28eef52
Ritabrata Paul <67918498+Ritabrata-Paul@users.noreply.github.com>
2022-08-10 12:19:27 +0000

    3nd commit

--- components/common/Post.js
+++ components/common/Post.js
@@ -1,17 +1,23 @@
+import Vote from '../feed/Vote'
+
 const style = {
   post : 'flex flex-col space-y-1 cursor-pointer',
-  wrapper : 'flex space-x-3 rounded bg-[#1a1a1b]/80'
+  wrapper : 'flex space-x-3 rounded bg-[#1a1a1b]/80 p-2 border border-[#343536]',
+  postTitle : 'text-lg font-medium text-[#D7DADC]',
+  postContent : 'text-sm font-light text-[#D7DADC]/80'
+  
 }
 
 const Post = () => {
   return (
     <div className = {style.wrapper}>
-      <div className = {style.post}>
-        <>
-        <h1>Post</h1>
-        <p1>Nothing</p1>
-        </>
-      </div>
+    <Vote />  
+    <div className = {style.post}>
+      
+      <h1 className = {style.postTitle}>How many aptitude questions are there in the aptitude test?</h1>
+      <p1 className = {style.postContent}>More than 1000 aptitude questions and answers for freshers who are preparing for interviews and placements after graduation and master degree courses. Many online aptitude tests and quizzes available to gauge your readiness to face the competitive exams.</p1>
+      
+    </div>
     </div>
   )
 }
--- components/feed/index.js
+++ components/feed/index.js
@@ -1,5 +1,6 @@
 import Post from '../common/Post'
 
+
 const Feed = () => {
   return(
     <Post/>
